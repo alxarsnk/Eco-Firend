ba47f45e771b51221e07688ed3c49422d015ad25
Alexandr Arsenyuk <arsenyukofficial@gmail.com>
2019-12-08 14:26:26 +0300

    Initial commit

---
+++ Eco-Friend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eco-Friend
+//
+//  Created by Александр Арсенюк on 08.12.2019.
+//  Copyright © 2019 Александр Арсенюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
